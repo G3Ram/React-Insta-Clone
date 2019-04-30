b63eb8cb6ef216a9b2a733f1a5896567d82e483a
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-04-29 23:04:20 -0500

    Substituted the icons instead of placeholder
    Substitued the actual icons instead of the place holders

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -5,12 +5,20 @@ function SearchBar() {
   return (
     <div className="header">
       <div className="headerContent">
-        <img src="../img/instagramLogos.png" className="instagramLogoImage" />
-        <input type="text" className="searchText" placeholder="Search" />
-        <img
-          src="../img/instagramAccessory.png"
-          className="instagramAccesory"
-        />
+        {/* <img src="../img/instagramLogos.png" className="instagramLogoImage" /> */}
+        <div className="headerSection">
+          <img src="../img/iconInstagram.png" className="instagramLogoImage" />
+          <div className="verticalLine" />
+          <img src="../img/instagramText.png" className="instagramTextImage" />
+        </div>
+        <div className="searchSection">
+          <input type="text" className="searchText" placeholder="Search" />
+        </div>
+        <div className="accessorySection">
+          <img src="../img/iconCompass.png" className="instagramAccesory" />
+          <img src="../img/iconHeart.png" className="instagramAccesory" />
+          <img src="../img/iconProfile.png" className="instagramAccesory" />
+        </div>
       </div>
     </div>
   );
